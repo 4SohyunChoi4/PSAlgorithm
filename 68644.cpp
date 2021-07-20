a5cf153243210485f91f58ccfa9fc86547aa9656
unknown <sagie.choi@gmail.com>
2021-07-21 00:40:57 +0900

    두 개 뽑아서 더하기(수정)

--- 68644.cpp
+++ 68644.cpp
@@ -1,3 +1,4 @@
+#include <set>
 #include <string>
 #include <vector>
 #include <algorithm>
@@ -5,17 +6,17 @@ using namespace std;
 
 vector<int> solution(vector<int> numbers) {
     vector<int> answer;
+    set<int> set;
     // 두개 수 더한다
     for(int i=0; i<numbers.size() ; i++)
     {
         for(int j=i+1; j<numbers.size(); j++){
-            answer.push_back(numbers[i] + numbers[j]);
+            set.insert(numbers[i] + numbers[j]);
         }
     }
-    
     //오름차순으로 배열
-    sort(answer.begin(),answer.end());
-    answer.erase(unique(answer.begin(), answer.end()), answer.end());
-
+   // sort(answer.begin(),answer.end());
+    //answer.erase(unique(answer.begin(), answer.end()), answer.end());
+    answer.assign(set.begin(), set.end());
     return answer;
 }
