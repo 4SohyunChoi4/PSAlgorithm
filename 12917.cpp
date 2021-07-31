89a6baff4baaac79b4b665ee49695f153d0287d9
unknown <sagie.choi@gmail.com>
2021-08-01 00:29:46 +0900

    문자열 내림차순으로 배치하기 (수정)

--- 12917.cpp
+++ 12917.cpp
@@ -4,14 +4,6 @@
 using namespace std;
 
 string solution(string s) {
-    string answer = "";
-    vector<char> vec;
-    for(int i=0 ; i<s.size(); i++){
-        vec.push_back(s[i]);
-    }
-    sort(vec.begin(), vec.end(), greater<char>());
-    for(int i=0 ; i<vec.size(); i++){
-        answer+=vec[i];
-    }
-    return answer;
+	sort(s.begin(), s.end(), greater<char>());
+	return s;
 }
